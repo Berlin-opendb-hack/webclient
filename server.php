89172bec48f523c436e99be58da3556913648ec7
Johannes <johannes.hildingsson@klickrent.com>
2016-10-29 12:52:28 +0200

    add dropdown

--- server.php
+++ server.php
@@ -3,7 +3,7 @@ require __DIR__ . '/vendor/autoload.php';
 
 use JsonRPC\Client;
 
-  $bitcoin = new Client('http://bitcoinrpc:44sCpMzWlXvVnKVlNW2V8LKtB0Lbvw2yBHOs5N8yoBNZu@127.0.0.1:18332/');
+  $bitcoin = new Client('http://rpc:rpc@127.0.0.1:18332/');
 
   echo "<pre>\n";
   print_r($bitcoin->listtransactions()); echo "\n";
